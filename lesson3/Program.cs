cab7a76370077fdae1dcddb0e8362346987308fa
Adeyemi Olayemi <Triumphantsam30@gmail.com>
2020-11-20 23:10:57 +0100

    Add project files.

---
+++ lesson3/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace lesson3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x = 20;
+            int y = 10;
+
+            Console.WriteLine("x == 20: {0}", x==20);
+            Console.WriteLine("y ==3: {0}", y == 3);
+            Console.WriteLine("x != 5: {0}", x != 5);
+            Console.WriteLine("y != 10: {0}", y != 10);
+            Console.WriteLine("x >= 50: {0}", x >= 50);
+            Console.WriteLine("y >= 9: {0}", y >= 9);
+        }
+    }
+}
